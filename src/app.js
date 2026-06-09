26def94b7a6cb5d0ab9e9d09921c6b55a43e5de4
Harsh <shivhareharsh027@gmail.com>
2026-06-09 12:10:47 +0530

    multiple rout handler

--- src/app.js
+++ src/app.js
@@ -4,32 +4,26 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res)=>{
-    res.send("hello world from /hello");
-})
+app.get(
+    '/user',
+    (req,res, next)=>{
+        console.log("responce 1 console")
+        next();
+    },
+    (req, res, next)=>{
+        console.log("responce 2 console");
+        res.send("responce 2");
+        next();
+    },
+    (req, res )=>{
+        console.log("responce 3 console");
+        res.send("responce 3");
+    }
+)
 
 
 
-app.get("/user", (req, res)=>{
-    res.send("hello get from user");
-})
 
-app.post("/user", (req, res)=>{
-    res.send("hello post from user");
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("hello delete")
-})
-
-app.patch("/user", (req,res)=>{
-    res.send("hello patch");
-})
-
-
-app.use("/test", (req, res)=>{
-    res.send("hello world from /test");
-})
 
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
